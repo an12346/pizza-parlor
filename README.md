1401b0aacca6319c83f0479f83cd87c5c8e70287
Anna Pittman <annaepittman@gmail.com>
2021-11-05 08:19:51 -0700

    create pizza constructor

---
+++ README.md
@@ -0,0 +1,18 @@
+Describe:
+Test:
+Code:
+Expected Output:
+
+
+
+
+
+
+
+
+1. Write out plan for business logic
+2. Create every function/object needed
+3. Test every function
+4. Write out plan for html
+5. write out plan for UI logic
+6. Write out readme
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+//Business Logic
+
+function Pizza (size, toppings) {
+    this.size = size 
+    this.toppings = [];
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//Objects:
+  //Constuctor for Pizza
+    //1. Property for size;
+    //2. Property for toppings: [];
+
+//Functions:
+  //1. Function to calculate price
+    //-Input for size selection
+    //-Input for toppings selection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI Logic
